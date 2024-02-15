e91477894fa906c3cbacbbc414e7480a4c0b7010
liyananabila <160108044+liyananabila@users.noreply.github.com>
2024-02-15 15:43:47 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Source Code Management</title>
+  </head>
+  <body>
+<h2>Source Code Management</h2>
+<table>
+  <tr>
+    <th>BIL</th>
+    <th>NAMA</th>
+  </tr>
+  <tr>
+    <td>1.</td>
+    <td>yana</td>
+  </tr>
+</table>
+  </body>
+</html>
